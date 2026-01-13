f69c2460fcfd6f9591c4cd9caa183ce971f81891
Asheesh Rawat <asheeshrawat48@gmail.com>
2026-01-13 11:40:24 +0530

    Add Navbar layout

--- src/landing_page/Navbar.js
+++ src/landing_page/Navbar.js
@@ -1,7 +1,64 @@
 import React from "react";
 
 function Navbar() {
-  return <h1>Nav</h1>;
+  return (
+    <nav
+      className="navbar navbar-expand-lg bg-body-tertiary  border-bottom  "
+      style={{ backgroundColor: "#FFF" }}
+    >
+      <div className="container-fluid p-2">
+        <a className="navbar-brand" href="#">
+          <img
+            src="media/images/logo.svg"
+            alt="logo"
+            style={{ width: "25%" }}
+          />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <form className="d-flex" role="search">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link " aria-current="page" href="#">
+                  Signup
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Product
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Pricing
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Support
+                </a>
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
