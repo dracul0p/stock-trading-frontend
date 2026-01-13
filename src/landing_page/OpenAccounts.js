1b7b4782baf96b99f98230f403cd1508234294a5
Asheesh Rawat <asheeshrawat48@gmail.com>
2026-01-13 14:53:08 +0530

    Add Footer layout

--- src/landing_page/OpenAccounts.js
+++ src/landing_page/OpenAccounts.js
@@ -7,7 +7,7 @@ function OpenAccount() {
     <div className="container p-5 mb-5">
       <div className="row text-center ">
         <h1 className="mt-5">IOpen a Zerodha account</h1>
-        <p className ="m-4">
+        <p className="m-4">
           Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and
           F&O trades.
         </p>
