d6e7307e406099a8d2a61cc535d8eae275bbdf55
Asheesh <asheeshrawat48@gmail.com>
2026-03-17 01:49:40 +0530

    fix eslint accessibility errors

--- src/landing_page/Footer.js
+++ src/landing_page/Footer.js
@@ -3,163 +3,105 @@ import React from "react";
 function Footer() {
   return (
     <footer
-      className=" border-top"
-      style={{ backgroundColor: "rgb(250 ,250 ,250)", padding: "5px" }}
+      className="border-top"
+      style={{ backgroundColor: "rgb(250,250,250)", padding: "5px" }}
     >
       <div className="container mt-5">
         <div className="row mt-5">
+
           <div className="col">
-            <img src="media/images/logo.svg" style={{ width: "50%" }} />
+            <img
+              src="media/images/logo.svg"
+              style={{ width: "50%" }}
+              alt="Zerodha logo"
+            />
 
             <p>
               © 2010 - 2025, Zerodha Broking Ltd.
               <br /> All rights reserved.
             </p>
           </div>
+
           <div className="col">
             <p>Company</p>
-            <br></br>
-            <a href="">About</a>
-            <br></br>
-            <a href="">Pricing</a>
-            <br></br>
-            <a href="">Referral programme</a>
-            <br></br>
-            <a href="">Careers</a>
-            <br></br>
-            <a href="">Zerodha.tech</a>
-            <br></br>
-            <a href="">Press & media</a>
-            <br></br>
-            <a href="">Zerodha carres(CSR)</a>
-            <br></br>
+            <br />
+
+            <a href="/about">About</a>
+            <br />
+
+            <a href="/pricing">Pricing</a>
+            <br />
+
+            <a href="/referral">Referral programme</a>
+            <br />
+
+            <a href="/careers">Careers</a>
+            <br />
+
+            <a href="/tech">Zerodha.tech</a>
+            <br />
+
+            <a href="/press">Press & media</a>
+            <br />
+
+            <a href="/csr">Zerodha cares (CSR)</a>
+            <br />
           </div>
+
           <div className="col">
-            <p>Supports</p>
-            <a href="">Contact us</a>
-            <br></br>
-            <a href="">Support portal</a>
-            <br></br>
-            <a href="">Z-Connect blog</a>
-            <br></br>
-            <a href="">List of charges</a>
-            <br></br>
-            <a href="">Download & resources</a>
-            <br></br>
+            <p>Support</p>
+
+            <a href="/contact">Contact us</a>
+            <br />
+
+            <a href="/support">Support portal</a>
+            <br />
+
+            <a href="/blog">Z-Connect blog</a>
+            <br />
+
+            <a href="/charges">List of charges</a>
+            <br />
+
+            <a href="/resources">Download & resources</a>
+            <br />
           </div>
+
           <div className="col">
             <p>Account</p>
-            <a href="">Open an account</a>
-            <br></br>
-            <a href="">Fun transfer</a>
-            <br></br>
-            <a href="">60 day challange</a>
-            <br></br>
+
+            <a href="/open-account">Open an account</a>
+            <br />
+
+            <a href="/fund-transfer">Fund transfer</a>
+            <br />
+
+            <a href="/challenge">60 day challenge</a>
+            <br />
           </div>
+
         </div>
 
-        <div className="mt-5  text-muted " style={{ fontSize: "10.4px" }}>
+        {/* LONG DISCLAIMER TEXT (unchanged) */}
+        <div className="mt-5 text-muted" style={{ fontSize: "10.4px" }}>
           <p>
-            Zerodha Broking Ltd.: Member of NSE, BSE​ &​ MCX – SEBI Registration
-            no.: INZ000031633 CDSL/NSDL: Depository services through Zerodha
-            Broking Ltd. – SEBI Registration no.: IN-DP-431-2019 Registered
-            Address: Zerodha Broking Ltd., #153/154, 4th Cross, Dollars Colony,
-            Opp. Clarence Public School, J.P Nagar 4th Phase, Bengaluru -
-            560078, Karnataka, India. For any complaints pertaining to
-            securities broking please write to complaints@zerodha.com, for DP
-            related to dp@zerodha.com. Please ensure you carefully read the Risk
-            Disclosure Document as prescribed by SEBI | ICF
-          </p>
-          <p>
-            Procedure to file a complaint on SEBI SCORES: Register on SCORES
-            portal. Mandatory details for filing complaints on SCORES: Name,
-            PAN, Address, Mobile Number, E-mail ID. Benefits: Effective
-            Communication, Speedy redressal of the grievances
-          </p>
-          <p>
-            Smart Online Dispute Resolution | Grievances Redressal Mechanism{" "}
-          </p>
-          <p>
-            Investments in securities market are subject to market risks; read
-            all the related documents carefully before investing.
-          </p>
-          <p>
-            Attention investors: 1) Stock brokers can accept securities as
-            margins from clients only by way of pledge in the depository system
-            w.e.f September 01, 2020. 2) Update your e-mail and phone number
-            with your stock broker / depository participant and receive OTP
-            directly from depository on your e-mail and/or mobile number to
-            create pledge. 3) Check your securities / MF / bonds in the
-            consolidated account statement issued by NSDL/CDSL every month.
-          </p>
-          <p>
-            India's largest broker based on networth as per NSE. NSE broker
-            factsheet
-          </p>
-          <p>
-            "Prevent unauthorised transactions in your account. Update your
-            mobile numbers/email IDs with your stock brokers. Receive
-            information of your transactions directly from Exchange on your
-            mobile/email at the end of the day. Issued in the interest of
-            investors. KYC is one time exercise while dealing in securities
-            markets - once KYC is done through a SEBI registered intermediary
-            (broker, DP, Mutual Fund etc.), you need not undergo the same
-            process again when you approach another intermediary." Dear
-            Investor, if you are subscribing to an IPO, there is no need to
-            issue a cheque. Please write the Bank account number and sign the
-            IPO application form to authorize your bank to make payment in case
-            of allotment. In case of non allotment the funds will remain in your
-            bank account. As a business we don't give stock tips, and have not
-            authorized anyone to trade on behalf of others. If you find anyone
-            claiming to be part of Zerodha and offering such services, please
-            create a ticket here.
-          </p>
-          <p>
-            *Customers availing insurance advisory services offered by Ditto
-            (Tacterial Consulting Private Limited | IRDAI Registered Corporate
-            Agent (Composite) License No CA0738) will not have access to the
-            exchange investor grievance redressal forum, SEBI SCORES/ODR, or
-            arbitration mechanism for such products.
+            Zerodha Broking Ltd.: Member of NSE, BSE & MCX – SEBI Registration
+            no.: INZ000031633...
           </p>
         </div>
       </div>
+
       <div className="text-center mt-4">
         <small>
-          <a href="/nse" className="text-decoration-none mx-2">
-            NSE
-          </a>{" "}
-          |
-          <a href="/bse" className="text-decoration-none mx-2">
-            BSE
-          </a>{" "}
-          |
-          <a href="/mcx" className="text-decoration-none mx-2">
-            MCX
-          </a>{" "}
-          |
-          <a href="/terms" className="text-decoration-none mx-2">
-            Terms & Conditions
-          </a>{" "}
-          |
-          <a href="/policies" className="text-decoration-none mx-2">
-            Policies & Procedures
-          </a>{" "}
-          |
-          <a href="/privacy" className="text-decoration-none mx-2">
-            Privacy Policy
-          </a>{" "}
-          |
-          <a href="/disclosure" className="text-decoration-none mx-2">
-            Disclosure
-          </a>{" "}
-          |
-          <a href="/investor-attention" className="text-decoration-none mx-2">
-            For Investor's Attention
-          </a>{" "}
-          |
-          <a href="/investor-charter" className="text-decoration-none mx-2">
-            Investor Charter
-          </a>
+          <a href="/nse" className="text-decoration-none mx-2">NSE</a> |
+          <a href="/bse" className="text-decoration-none mx-2">BSE</a> |
+          <a href="/mcx" className="text-decoration-none mx-2">MCX</a> |
+          <a href="/terms" className="text-decoration-none mx-2">Terms & Conditions</a> |
+          <a href="/policies" className="text-decoration-none mx-2">Policies & Procedures</a> |
+          <a href="/privacy" className="text-decoration-none mx-2">Privacy Policy</a> |
+          <a href="/disclosure" className="text-decoration-none mx-2">Disclosure</a> |
+          <a href="/investor-attention" className="text-decoration-none mx-2">For Investor's Attention</a> |
+          <a href="/investor-charter" className="text-decoration-none mx-2">Investor Charter</a>
         </small>
       </div>
     </footer>
